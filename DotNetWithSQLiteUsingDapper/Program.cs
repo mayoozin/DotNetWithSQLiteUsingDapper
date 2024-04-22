fac18395d47783ad857e6d8f998aaacd30bae286
mayoo <mayoozin.developer@gmail.com>
2024-04-22 11:52:51 +0630

    -[ Add ] Service Registraion

--- DotNetWithSQLiteUsingDapper/Program.cs
+++ DotNetWithSQLiteUsingDapper/Program.cs
@@ -1,13 +1,44 @@
+using DotNetWithSQLiteUsingDapper;
+using DotNetWithSQLiteUsingDapper.DbServices;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddSingleton<DataContext>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(x =>
+{
+    // serialize enums as strings in api responses (e.g. Role)
+    x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
 
+    // ignore omitted parameters on models to enable optional params (e.g. User update)
+    x.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+});
+
+builder.Services.AddInfrastructure();
+builder.Services.AddScoped(n =>
+{
+    string folderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Sqlite");
+    Directory.CreateDirectory(folderPath);
+    //string filePath = Path.Combine(folderPath, builder.Configuration.GetSection("DbFileName").Value!);
+    //string connectionString = $"Data Source={filePath};Version=3;";
+    string connectionString = "Data Source=Blog.db;Version=3;";
+    return new DataContext(connectionString);
+});
 var app = builder.Build();
+
+//{
+//    using var scope = app.Services.CreateScope();
+//    var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+//    await context.Init();
+//}
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
