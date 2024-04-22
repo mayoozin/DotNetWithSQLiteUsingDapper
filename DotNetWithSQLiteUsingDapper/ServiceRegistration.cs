997ba2cf2604328b614ad8b39dd7a66fde1156de
mayoo <mayoozin.developer@gmail.com>
2024-04-22 11:49:49 +0630

    - [ Added ] Service Registration

---
+++ DotNetWithSQLiteUsingDapper/ServiceRegistration.cs
@@ -0,0 +1,16 @@
+﻿using DotNetWithSQLiteUsingDapper.DbServices;
+using DotNetWithSQLiteUsingDapper.Services;
+
+namespace DotNetWithSQLiteUsingDapper
+{
+    public static class ServiceRegistration
+    {
+        public static void AddInfrastructure(this IServiceCollection services)
+        {
+            // configure DI for application services
+            //services.AddScoped<IBlogRepository, BlogRepository>();
+            services.AddScoped<IBlogService, BlogService>();
+        }
+    }
+
+}
