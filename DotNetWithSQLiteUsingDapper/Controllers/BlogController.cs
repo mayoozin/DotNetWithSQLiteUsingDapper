8c4c93b34976aedcb2fa0171c05e05a48bd55ceb
mayoo <mayoozin.developer@gmail.com>
2024-04-22 10:20:37 +0630

    Add project files.

---
+++ DotNetWithSQLiteUsingDapper/Controllers/BlogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetWithSQLiteUsingDapper.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
