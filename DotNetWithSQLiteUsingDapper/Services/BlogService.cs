22cf3a835691d7bf53cc85fb9dfa3cd62d3fb549
mayoo <mayoozin.developer@gmail.com>
2024-04-22 11:52:04 +0630

    - [ Modified ] Blog services

---
+++ DotNetWithSQLiteUsingDapper/Services/BlogService.cs
@@ -0,0 +1,24 @@
+﻿using DotNetWithSQLiteUsingDapper.Model;
+
+namespace DotNetWithSQLiteUsingDapper.Services
+{
+    public interface IBlogService
+    {
+        Task<IEnumerable<BlogModel>> GetAll();
+        //Task<BlogModel> GetById(int id);
+        //Task Create(CreateRequest model);
+        //Task Update(int id, UpdateRequest model);
+        //Task Delete(int id);
+    }
+    public class BlogService : IBlogService
+    {
+
+        public BlogService()
+        {
+        }
+        public async Task<IEnumerable<BlogModel>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
