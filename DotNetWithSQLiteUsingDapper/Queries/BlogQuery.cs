f2adb026169db059034e3df41c778c19e81db4a0
mayoo <mayoozin.developer@gmail.com>
2024-04-22 11:52:16 +0630

    - [ Added ] Blog Query

---
+++ DotNetWithSQLiteUsingDapper/Queries/BlogQuery.cs
@@ -0,0 +1,29 @@
+﻿namespace DotNetWithSQLiteUsingDapper.Queries
+{
+    public class BlogQuery
+    {
+        public static string CreateBlogTable =
+   @"CREATE TABLE IF NOT EXISTS Tbl_Blog 
+    (BlogId TEXT NOT NULL, 
+    BlogTitle TEXT NOT NULL, 
+    BlogAuthor TEXT NOT NULL, 
+    BlogContent TEXT NOT NULL)";
+
+
+        public static string Insert =
+   @"INSERT INTO Tbl_Blog (BlogId, BlogTitle, BlogAuthor, BlogContent) 
+    VALUES (@BlogId, @BlogTitle, @BlogAuthor, @BlogContent)";
+
+        public static string GetAll = @"SELECT * FROM Tbl_Blog";
+
+        public static string Update = @"UPDATE Tbl_Blog SET 
+                                            BlogTitle = @BlogTitle,
+                                            BlogAuthor = @BlogAuthor,
+                                            BlogContent = @BlogContent 
+                                            WHERE BlogId = @BlogId";
+
+        public static string Delete = @"DELETE FROM Tbl_Blog WHERE BlogId = @BlogId";
+
+        public static string GetDataById = @"SELECT * FROM Tbl_BLog WHERE BlogId = @BlogId";
+    }
+}
