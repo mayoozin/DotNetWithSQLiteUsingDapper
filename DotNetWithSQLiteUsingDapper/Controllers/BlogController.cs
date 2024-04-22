526717a261462fda3f950f21abafa7c99358a0c2
mayoo <mayoozin.developer@gmail.com>
2024-04-22 10:24:07 +0630

    - [ Added ] New Blog Controller

--- DotNetWithSQLiteUsingDapper/Controllers/BlogController.cs
+++ DotNetWithSQLiteUsingDapper/Controllers/BlogController.cs
@@ -1,12 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DotNetWithSQLiteUsingDapper.Controllers
 {
-    public class BlogController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
     }
 }
