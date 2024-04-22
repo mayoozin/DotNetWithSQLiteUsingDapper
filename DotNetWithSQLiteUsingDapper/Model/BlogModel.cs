7a117ee1726f58683a7bb2076d0725badaa0cc50
mayoo <mayoozin.developer@gmail.com>
2024-04-22 10:23:49 +0630

    - [ Added ] Blog Model

---
+++ DotNetWithSQLiteUsingDapper/Model/BlogModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNetWithSQLiteUsingDapper.Model
+{
+    public class BlogModel
+    {
+        public string? BlogId { get; set; }
+        public string? BlogTitle { get; set; }
+        public string? BlogAuthor { get; set; }
+        public string? BlogContent { get; set; }
+    }
+}
