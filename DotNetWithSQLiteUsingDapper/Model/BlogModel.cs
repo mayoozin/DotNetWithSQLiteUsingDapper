f14a036e966542d0670bfb5afb14e3f56be1fab4
mayoo <mayoozin.developer@gmail.com>
2024-04-28 01:05:36 +0630

    - [ Updated ] Model

--- DotNetWithSQLiteUsingDapper/Model/BlogModel.cs
+++ DotNetWithSQLiteUsingDapper/Model/BlogModel.cs
@@ -14,4 +14,11 @@
         public string? BlogAuthor { get; set; }
         public string? BlogContent { get; set; }
     }
+    public class BlogUpdateModel
+    {
+        public int BlogId { get; set; }
+        public string? BlogTitle { get; set; }
+        public string? BlogAuthor { get; set; }
+        public string? BlogContent { get; set; }
+    }
 }
